3fa4d68862afc30f8dad7aba1852477250a97456
Renan Santos <renansantos@Renans-MacBook-Pro.local>
2023-10-19 16:03:13 -0300

    feat: Added context on server and client

--- client/client.go
+++ client/client.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Quotation struct {
@@ -15,45 +17,63 @@ type Quotation struct {
 }
 
 func main() {
-	// Make call to server.go to make dollar quotation only field BID.
-	dollarQuotation := GetDollarQuotationFromServer()
 
 	// Context: 300ms to call server.
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	// Make call to server.go to make dollar quotation only field BID.
+	dollarQuotation, err := GetDollarQuotationFromServer(ctx)
+	if err != nil {
+		fmt.Println("ERROR on request: ", err)
+		return
+	}
+
 	// Save response on cotacao.txt, on format: Dólar:{valor}
-	SaveOnFile(dollarQuotation)
+	err = SaveOnFile(dollarQuotation)
+	if err != nil {
+		fmt.Println("ERROR on save file: ", err)
+		return
+	}
 }
 
-func GetDollarQuotationFromServer() Quotation {
-	req, err := http.Get("http://localhost:8080/cotacao")
+func GetDollarQuotationFromServer(ctx context.Context) (Quotation, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		fmt.Println("ERROR:", err)
+		return Quotation{}, err
 	}
-	defer req.Body.Close()
-
-	res, err := io.ReadAll(req.Body)
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println("ERROR:", err)
+		return Quotation{}, err
+	}
+	defer res.Body.Close()
+
+	resp, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Quotation{}, err
 	}
 
 	var quotation Quotation
-	err = json.Unmarshal(res, &quotation)
+	err = json.Unmarshal(resp, &quotation)
 	if err != nil {
-		fmt.Println("ERROR:", err)
+		return Quotation{}, err
 	}
 
-	return quotation
+	fmt.Print("Returned request server:" + string(resp))
+	return quotation, nil
 }
 
-func SaveOnFile(dolar Quotation) {
+func SaveOnFile(dolar Quotation) error {
 	file, err := os.Create("cotacao.txt")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer file.Close()
 
 	_, err = file.WriteString("Dólar: " + dolar.Usdbrl.Bid)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	fmt.Println("Saved quotation on file.")
+	return nil
 }
